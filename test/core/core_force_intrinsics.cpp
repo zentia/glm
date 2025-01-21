97995f4713171ebc5bb722fe8141133ede45eecc
christophe <christophe@g-truc.net>
2025-01-22 00:36:32 +0100

    Fix intrinsics test

--- test/core/core_force_intrinsics.cpp
+++ test/core/core_force_intrinsics.cpp
@@ -1,4 +1,6 @@
-#define GLM_FORCE_INTRINSICS
+#ifndef GLM_FORCE_INTRINSICS
+#	define GLM_FORCE_INTRINSICS
+#endif//GLM_FORCE_INTRINSICS
 #define GLM_FORCE_SWIZZLE
 #include <glm/ext/scalar_constants.hpp>
 #include <glm/ext/vector_relational.hpp>
